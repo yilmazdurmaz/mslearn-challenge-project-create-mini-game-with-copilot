ce57528cf11620b7e40206f42a53fe96b37188b1
Yılmaz Durmaz <yilmaz_durmaz@hotmail.com>
2023-12-20 18:08:50 +0000

    rewrite to specs, again ms copilot

--- app.py
+++ app.py
@@ -9,24 +9,37 @@ print("hello world")  # this should have been filled by CoPilot
 # -At each round, the player must enter one of the options in the list and be informed if they won, lost, or tied with the opponent.
 # -By the end of each round, the player can choose whether to play again.
 # -Display the player's score at the end of the game.
-#- The minigame must handle user inputs, putting them in lowercase and informing the user if the option is invalid.
+# -The minigame must handle user inputs, putting them in lowercase and informing the user if the option is invalid.
+# PROMPT: use it as a specification sheet to create a python program
 
 import random
 
 def play_game():
     print("Welcome to Rock, Paper, Scissors!")
-    print("Enter your choice: 'rock', 'paper', or 'scissors'")
-    user_choice = input().lower()
-    computer_choice = random.choice(['rock', 'paper', 'scissors'])
-    print(f"\nYou chose {user_choice}, and the computer chose {computer_choice}.\n")
-    if user_choice == computer_choice:
-        print("It's a tie!")
-    elif (user_choice == 'rock' and computer_choice == 'scissors') or \
-         (user_choice == 'paper' and computer_choice == 'rock') or \
-         (user_choice == 'scissors' and computer_choice == 'paper'):
-        print("You win!")
-    else:
-        print("The computer wins!")
+    options = ['rock', 'paper', 'scissors']
+    score = {'player': 0, 'computer': 0, 'tie': 0}
+    while True:
+        print(f"\nScore: Player {score['player']} - Computer {score['computer']} - Tie {score['tie']}")
+        user_choice = input("Enter your choice: 'rock', 'paper', or 'scissors' (or 'quit' to exit): ").lower()
+        if user_choice == 'quit':
+            break
+        elif user_choice not in options:
+            print("Invalid choice. Please enter 'rock', 'paper', or 'scissors'.")
+            continue
+        computer_choice = random.choice(options)
+        print(f"\nYou chose {user_choice}, and the computer chose {computer_choice}.\n")
+        if user_choice == computer_choice:
+            print("It's a tie!")
+            score['tie'] += 1
+        elif (user_choice == 'rock' and computer_choice == 'scissors') or \
+             (user_choice == 'paper' and computer_choice == 'rock') or \
+             (user_choice == 'scissors' and computer_choice == 'paper'):
+            print("You win!")
+            score['player'] += 1
+        else:
+            print("The computer wins!")
+            score['computer'] += 1
+    print(f"\nFinal score: Player {score['player']} - Computer {score['computer']} - Tie {score['tie']}")
     print("\nThanks for playing!")
 
 play_game()
